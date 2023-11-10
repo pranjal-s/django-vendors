51c3036be68ee80b15f9582185d8e906857e7812
Pranjal Singh <pranjal.officemail@gmail.com>
2023-11-10 19:20:03 +0530

    templates setup

--- vendors_tutorial/vendors_tutorial/settings.py
+++ vendors_tutorial/vendors_tutorial/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'vendors_tutorial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
